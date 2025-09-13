90327badeaacd2a06a6e87df0df89057b274b294
Leont1cov <chuchunovvakov@mail.ru>
2025-09-13 13:30:12 +0300

    feat: move cards data to a separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 import CreateCard from "./components/CreateCard";
-import TodoCard from "./components/TodoCard/TodoCard";
 import TodoList from "./components/TodoList/TodoList";
+import { cards } from "./components/data/cards";
 
 function App() {
-    const cards = [
-        { id: 1, title: "JavaScript" },
-        { id: 2, title: "HTML CSS" },
-        { id: 3, title: "C++" },
-    ];
-
     return (
         <>
             <h1 style={{ display: "flex", justifyContent: "center" }}>

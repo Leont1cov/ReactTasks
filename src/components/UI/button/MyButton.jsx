187f374186edba7eedb95bf8dd5822367de6763e
Leont1cov <chuchunovvakov@mail.ru>
2025-09-13 11:28:54 +0300

    feat: add button component

---
+++ src/components/UI/button/MyButton.jsx
@@ -0,0 +1,11 @@
+import cl from "./MyButton.module.css";
+
+const MyButton = ({ children, props }) => {
+    return (
+        <button className={cl.MyButton} {...props}>
+            {children}
+        </button>
+    );
+};
+
+export default MyButton;

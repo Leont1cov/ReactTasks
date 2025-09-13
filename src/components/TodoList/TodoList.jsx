c3447979a1d25b40e7f91fc36853c71cbefe1307
Leont1cov <chuchunovvakov@mail.ru>
2025-09-13 13:30:42 +0300

    fix: update TodoList map to use index as key

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,8 +4,8 @@ import cl from "../TodoList/TodoList.module.css";
 const TodoList = ({ cards }) => {
     return (
         <div className={cl.list}>
-            {cards.map((element) => {
-                return <TodoCard key={element.id} card={element.title} />;
+            {cards.map((element, index) => {
+                return <TodoCard key={index + 1} card={element.title} />;
             })}
         </div>
     );

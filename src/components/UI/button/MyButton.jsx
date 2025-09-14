8b04d7e3371f83499641f270cea371e076d9bb93
Leont1cov <chuchunovvakov@mail.ru>
2025-09-14 22:16:36 +0300

    feat: add clsx

--- src/components/UI/button/MyButton.jsx
+++ src/components/UI/button/MyButton.jsx
@@ -1,8 +1,13 @@
 import cl from "./MyButton.module.css";
+import clsx from "clsx";
 
-const MyButton = ({ children, props, onClick }) => {
+const MyButton = ({ children, onClick, className, props }) => {
     return (
-        <button onClick={onClick} className={cl.MyButton} {...props}>
+        <button
+            onClick={onClick}
+            className={clsx(cl.MyButton, className)}
+            {...props}
+        >
             {children}
         </button>
     );

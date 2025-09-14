2cd94db2fb0abba07d9638b58058c6231a88128a
Leont1cov <chuchunovvakov@mail.ru>
2025-09-14 22:44:49 +0300

    feat: add button deleate component

--- src/components/TodoCard/TodoCard.jsx
+++ src/components/TodoCard/TodoCard.jsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
 import "./TodoCard.css";
+import MyButton from "../UI/button/MyButton";
+import clsx from "clsx";
 
 const TodoCard = ({ card }) => {
     const [checked, setChecked] = useState(false);
 
     return (
-        <div className={checked ? "card card-done" : "card"}>
+        <div className={clsx("card", { " card-done": checked })}>
             <input onClick={() => setChecked(!checked)} type="checkbox" />
             <span className={checked ? "card-checked" : ""}>{card}</span>
+            <MyButton
+                className={clsx("card-deleate", {
+                    " card-deleate-border": checked,
+                })}
+            >
+                Deleate
+            </MyButton>
         </div>
     );
 };
